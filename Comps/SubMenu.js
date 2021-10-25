7070219ca05d1e2460b2926b287d9069210b9b01
Abhay <abakshi4@my.bcit.ca>
2021-10-24 19:12:20 -0700

    fixing the props.js

--- Comps/SubMenu.js
+++ Comps/SubMenu.js
@@ -5,10 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import styled from 'styled-components/native'
 
+import { Configurations } from '../PropConfig/Props';
+
+const secCol = Configurations.colors.secCol
+
 const MenuCont = styled.View`
 height:50%;
 width:25%;
-background-color:blue;
+background-color:${props => props.color};
 `
 const IconCell = styled.Pressable`
 width:100%;
@@ -19,9 +23,13 @@ width:100%;
 `  
 
 
-export const SubMenu = ()=> {
+export const SubMenu = (
+    {
+        color = Configurations.colors.secCol
+    }
+)=> {
     return (
-        <MenuCont>
+        <MenuCont color = {color}>
             <IconCell> 
                 <FontAwesomeIcon icon={faCoffee} size={35} color="white"/> 
             </IconCell>
