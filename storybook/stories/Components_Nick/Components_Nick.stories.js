84836bd24e58251dadabf9c856d37aa3f7e32ae8
Nick Barr <nbarr2@my.bcit.ca>
2021-10-25 22:29:01 -0700

    added Group Event Card component

--- storybook/stories/Components_Nick/Components_Nick.stories.js
+++ storybook/stories/Components_Nick/Components_Nick.stories.js
@@ -3,9 +3,11 @@ import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import CenterView from '../CenterView';
 import NavBar from '../../../comps/NavBar';
+import GroupEventCard from '../../../comps/GroupEventCard';
 
 storiesOf('Components_Nick', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('NavBar', () => <NavBar/>)
+  .add('GroupEventCard', () => <GroupEventCard/>)
   
   
