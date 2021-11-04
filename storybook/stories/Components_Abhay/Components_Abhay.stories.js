7cfe98985f5830530352fd3691c4b7ddf294926a
Abhay <abakshi4@my.bcit.ca>
2021-11-03 21:56:11 -0700

    taking changes from abhayTest branch

--- storybook/stories/Components_Abhay/Components_Abhay.stories.js
+++ storybook/stories/Components_Abhay/Components_Abhay.stories.js
@@ -6,7 +6,9 @@ import { SubMenu } from '../../../comps/SubMenu';
 import {Task} from '../../../comps/Task'
 import {ChatBar} from '../../../comps/ChatBar'
 import {CreateGroup} from  '../../../comps/CreateGroup'
-import {Calendar} from '../../../comps/Calendar'
+import {CalAgenda} from '../../../comps/Agenda'
+import {HomeCalendar} from '../../../comps/Calendar'
+import {TimeCalendar} from '../../../comps/TimeCalendar'
 
 storiesOf('Components_Abhay', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
@@ -14,7 +16,9 @@ storiesOf('Components_Abhay', module)
   .add('statusSubMenu', () => <SubMenu/>)
   .add('Chat Bar', () => <ChatBar/>)
   .add ('Create group', ()=> <CreateGroup/>)
-  .add ('Calendar', ()=> <Calendar/>)
+  .add ('CalAgenda', ()=> <CalAgenda/>)
+  .add ('HomeCalendar', ()=> <HomeCalendar/>)
+  .add ('TimeCalendar', ()=> <TimeCalendar/>)
  
 
   
