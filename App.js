2066bb4d913c9421d4ff69a5a8a74168ba1e3de3
Nick Barr <nbarr2@my.bcit.ca>
2021-11-08 18:34:24 -0800

    staging App.js

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-=======
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TaskCreatingScreen from './screens/TaskCreatingScreen';
 import TaskboardScreen from './screens/TaskboardScreen '
->>>>>>> origin/levi
+
 
 // export {default}from './storybook'
 
