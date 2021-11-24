e52c80cd8c0d5487f7a9683ae70636f9de720ce0
Abhay <abakshi4@my.bcit.ca>
2021-11-24 13:12:32 -0800

    commit before fetching latest changes

--- screens/TaskboardScreen .js
+++ screens/TaskboardScreen .js
@@ -171,7 +171,7 @@ const TaskboardScreen = ({ navigation }) => {
   <TaskCardArea/> 
      
     
-  {welcome ? <Text style={styles.title}>Welcome  {users.name}!</Text>: null  }
+  {welcome ? <Text style={styles.title}>Welcome {users.name} !</Text>: null  }
    
 
      
