741508a3363dc4a97ab9060e978b6011d971c037
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2021-11-24 19:54:46 -0800

    sovled crush issue

--- navigation/TaskStack.js
+++ navigation/TaskStack.js
@@ -25,6 +25,8 @@ import ScheduleMeetingStepTwoScreen from '../screens/ScheduleMeetingStepTwo';
 import ScheduleMeetingStepThreeScreen from '../screens/ScheduleMeetingStepThree';
 import ScheduleMeetingStepFourScreen from '../screens/ScheduleMeetingStepFour';
 import ScheduleMeetingStepFiveScreen from '../screens/ScheduleMeetingStepFive';
+import { useNavigation } from '@react-navigation/native';
+import {Entypo} from '@expo/vector-icons'
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -121,7 +123,7 @@ export default function TaskboardStack() {
         
         headerRight: () => (
          
-          <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+          <TouchableOpacity onPress={() => alert('Swide left')}>
             <Entypo name="menu" size={30} color="lightgrey" />
           </TouchableOpacity>
         ),
