4b99e7cdb44867d2a76f0bd6397566d1608736e0
levic559 <ychen559@my.bcit.ca>
2021-12-01 22:50:07 -0800

    igore error logbox

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -26,7 +26,9 @@ import { Events } from '../data/Events';
 import {GroupEventCard} from '../comps/GroupEventCard';
 import { AuthenticatedUserContext } from '../navigation/AuthenticatedUserProvider';
 
-
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
 const colors = Configurations.colors;
 const secCol = colors.secCol;
 const accent = colors.butCol;
